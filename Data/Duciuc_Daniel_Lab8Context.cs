cdcb10e286ddfbd04935e52bb371ee703fb8d366
DuciucD <duciuc_daniel@ymail.com>
2022-01-23 12:08:37 +0200

    Add project files.

---
+++ Data/Duciuc_Daniel_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Duciuc_Daniel_Lab8.Models;
+
+namespace Duciuc_Daniel_Lab8.Data
+{
+    public class Duciuc_Daniel_Lab8Context : DbContext
+    {
+        public Duciuc_Daniel_Lab8Context (DbContextOptions<Duciuc_Daniel_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Duciuc_Daniel_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Duciuc_Daniel_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Duciuc_Daniel_Lab8.Models.Category> Category { get; set; }
+    }
+}
